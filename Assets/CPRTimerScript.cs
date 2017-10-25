3e397d214f40251084ea2c733ab2617448779b99
Luke Nittmann <lnittman@umich.edu>
2017-10-24 18:51:57 -0700

    Back button implemented

--- Assets/CPRTimerScript.cs
+++ Assets/CPRTimerScript.cs
@@ -4,11 +4,12 @@ using UnityEngine.UI;
 public class CPRTimerScript : MonoBehaviour {
     
     public Text timerText;
-    private float timer;
     private Canvas canvas;
-    private bool wasEnabled;
     private Button nextButton;
-  
+    private Button backButton;
+    private bool wasEnabled;
+    private float timer;
+
     void Start() {
          
         timer = 120;
@@ -19,6 +20,9 @@ public class CPRTimerScript : MonoBehaviour {
         GameObject card = timerText.transform.parent.gameObject.transform.parent.gameObject;
         nextButton = card.transform.Find("Buttons/Footer").GetComponent<Button>();
         nextButton.onClick.AddListener(NextClicked);
+
+        backButton = card.transform.Find("Buttons/Back").GetComponent<Button>();
+        backButton.onClick.AddListener(NextClicked);
     }
 
     void Update () {
