1fb8c6800b30f5cfd22c4ca880fee43094dfe282
Luke Nittmann <lnittman@umich.edu>
2017-10-26 15:59:20 -0700

    added scripts to follow origin canvas of detail buttons
    Duplicate cards for CPR-QUALITY and SHOCKABLE-RHYTHMS aren't needed anymore

---
+++ Assets/CPRQualityDetailRouteScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CPRQualityDetailRouteScript : MonoBehaviour
+{
+    private Canvas origin;
+    private Button detailButton;
+    private Button detailCloseButton;
+
+    void Start()
+    {
+        origin = GetComponentInParent<Canvas>();
+
+        detailButton = GetComponent<Button>();
+        detailButton.onClick.AddListener(DetailClicked);
+
+        GameObject goButtons = detailButton.transform.parent.gameObject;
+        GameObject goCards = goButtons.transform.parent.gameObject.transform.parent.gameObject;
+        detailCloseButton = goCards.transform.Find("CPR-QUALITY-DETAIL/Buttons/Footer").GetComponent<Button>();
+    }
+
+    void DetailClicked()
+    {
+        detailCloseButton.onClick.RemoveAllListeners();
+        detailCloseButton.onClick.AddListener(SetCanvas);
+    }
+
+    void SetCanvas()
+    {
+        origin.enabled = true;
+    }
+}
---
+++ Assets/ShockableRythmsDetailRouteScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShockableRythmsDetailRouteScript : MonoBehaviour
+{
+    private Canvas origin;
+    private Button detailButton;
+    private Button detailCloseButton;
+
+    void Start()
+    {
+        origin = GetComponentInParent<Canvas>();
+
+        detailButton = GetComponent<Button>();
+        detailButton.onClick.AddListener(DetailClicked);
+
+        GameObject goButtons = detailButton.transform.parent.gameObject;
+        GameObject goCards = goButtons.transform.parent.gameObject.transform.parent.gameObject;
+        detailCloseButton = goCards.transform.Find("SHOCKABLE-RHYTHMS-DETAIL/Buttons/Footer").GetComponent<Button>();
+    }
+
+    void DetailClicked()
+    {
+        detailCloseButton.onClick.RemoveAllListeners();
+        detailCloseButton.onClick.AddListener(SetCanvas);
+    }
+
+    void SetCanvas()
+    {
+        origin.enabled = true;
+    }
+}
