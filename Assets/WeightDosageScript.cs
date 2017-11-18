55d108b443ae2e392608f6a2b1d38932a348f47c
Eric Crews <ecrews@umich.edu>
2017-11-18 12:57:30 -0500

    Revert to c3da8f3

--- Assets/WeightDosageScript.cs
+++ Assets/WeightDosageScript.cs
@@ -5,10 +5,6 @@ using UnityEngine.UI;
 
 public class WeightDosageScript : MonoBehaviour {
 
-    private Canvas title;
-    private Canvas cpr_start;
-    private Text inputLabel;
-    
     private Button startButton;
     private GameObject goWeightInput;
     private double weight;
@@ -30,13 +26,8 @@ public class WeightDosageScript : MonoBehaviour {
         startButton.onClick.AddListener(setDosages);
 
         goWeightInput = startButton.transform.parent.gameObject;
-        inputLabel = goWeightInput.transform.Find("Label").GetComponent<Text>();
         GameObject goCards = goWeightInput.transform.parent.gameObject.transform.parent.gameObject;
 
-        title = goCards.transform.Find("TITLE").GetComponent<Canvas>();
-        cpr_start = goCards.transform.Find("CPR-1").GetComponent<Canvas>();       
- 
-        // grab all dosage text objects
         shock_3 = goCards.transform.Find("SHK-3/Text/Subheader").GetComponent<Text>();
         shock_5 = goCards.transform.Find("SHK-5/Text/Subheader").GetComponent<Text>();
         shock_5_2 = goCards.transform.Find("SHK-5-2/Text/Subheader").GetComponent<Text>();
@@ -49,12 +40,9 @@ public class WeightDosageScript : MonoBehaviour {
 
     void setDosages()
     {
-        weight = int.Parse(goWeightInput.transform.Find("InputField/Text").GetComponent<Text>().text);
-        if (weight < 0)
-        {
-            inputLabel.text = "Please enter a non-negative patient weight in kg";
-            return;
-        }
+        weight = int.Parse(goWeightInput
+                 .transform.Find("InputField/Text")
+                 .GetComponent<Text>().text);
 
         //Set Shock dosages
         shock_3.text = "• " + calculateDosage(2) + " J";
@@ -68,12 +56,9 @@ public class WeightDosageScript : MonoBehaviour {
         epinephrine.text = "• " + calculateDosage(0.01) + " mg. Repeat every 3-5min\n• If no IO / IV access, may give endotracheal dose: " + calculateDosage(0.1) + " mg";
         amiodarone.text = "• " + calculateDosage(5) + " mg bolus during cardiac arrest\n• May repeat up to 2 times for refractory VF/ pulseless VT";
         lidocaine.text = "• Initial: " + calculateDosage(1) + " mg loading dose\n• Maintainence: [" + calculateDosage(20) + ", " + calculateDosage(50) + "] mcg per minute infusion\n(repeat bolus dose if infusion initiated > 15min after initial bolus therapy)";
-
-        title.enabled = false;
-        cpr_start.enabled = true;
     }
 
-    private string calculateDosage(double dosagePerKG)
+    string calculateDosage(double dosagePerKG)
     {
         return (dosagePerKG * weight).ToString();
     }
