926d83a697f1d1ea64afec37d22ddc7cf9a67e6a
timrayrichards <timrayrichards@gmail.com>
2018-04-09 19:02:09 -0400

    Reformat end screen

--- Assets/Scripts/States/End.cs
+++ Assets/Scripts/States/End.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class End : State
 {
     public GameObject end_window;
     private GameObject tutorial_text;
+    private Button rosc_btn;
 
     public override void Awake()
     {
@@ -11,12 +13,29 @@ public class End : State
         
         InitWindow(end_window);
 
-        State next_state = gameObject.GetComponent<Title>();
+        State next_state = gameObject.GetComponent<CPR10>();
         AddTransition(next_state, Utility.TransitionType.Next);
 
+        Button start_btn = window.transform.Find("Nav/StartScreen").GetComponent<Button>();
+        start_btn.onClick.AddListener(StartScreenTransition);
+
+        AddStaticDetailWindows();
+ 
         tutorial_text = GameObject.Find("ScreenOverlay/Tutorial");
 
         window.SetActive(false);  
+    }
+
+    private void StartScreenTransition()
+    {
+        Transition(gameObject.GetComponent<Title>(), Utility.TransitionType.Next);
+    }
+
+    private void AddStaticDetailWindows()
+    {
+        rosc_btn = window.transform.Find("DetailButtons/Rosc").GetComponent<Button>();
+        GameObject Rosc_window = GameObject.Find("Views/Details/Rosc");
+        SetupDetailListeners(rosc_btn, Rosc_window);
     }
 
     protected override void TransitionedTo(State prev_state, Utility.TransitionType type)
@@ -24,12 +43,16 @@ public class End : State
         if (tutorial_text.activeSelf)
         {
             prev_btn.interactable = false;
-            Speak("Helpful message. Air tap title button to go back to the main menu.");
+            rosc_btn.interactable = false;
+            next_btn.interactable = false;
+            Speak("This is the conclusion of the tutorial. Either air tap the start screen button.");
         }
         else
         {
             prev_btn.interactable = true;
-            Speak("Helpful message.");
+            rosc_btn.interactable = true;
+            next_btn.interactable = true;
+            Speak("Check pulse. If pulse present administer post-cardiac care. If asystole either air tap the asystole button, or say next.");
         }
     }
 }
